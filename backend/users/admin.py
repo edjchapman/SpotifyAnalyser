b6e6f7eff3614b809adc1659d933a3d2d2f72456
Ed Chapman <edchapman88@gmail.com>
2024-07-25 02:32:59 +0100

    Ft: Add a user model admin.

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,1 +1,20 @@
-# Register your models here.
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+
+from users.models import User
+
+
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    """
+    Custom User Model Admin
+    """
+
+    model = User
+    list_display = ["username", "email", "first_name", "last_name", "is_staff", "spotify_id"]
+    fieldsets = UserAdmin.fieldsets + (
+        (None, {"fields": ("spotify_id", "access_token", "refresh_token", "token_expiry")}),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (None, {"fields": ("spotify_id", "access_token", "refresh_token", "token_expiry")}),
+    )
