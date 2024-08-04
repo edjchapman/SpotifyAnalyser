1cae564f6cd5a3318278897f99203a32f7fca56b
Ed Chapman <edchapman88@gmail.com>
2024-08-04 23:39:52 +0100

    Add root URL redirect
    - Corrected import statements in accounts/urls.py by using absolute import instead of relative import.
    - Added RedirectView to core/urls.py to redirect the root URL to the accounts profile page.

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from .views import RegisterView, LoginView, ProfileView, CustomLogoutView
+from accounts.views import RegisterView, LoginView, ProfileView, CustomLogoutView
 
 urlpatterns = [
     path("register/", RegisterView.as_view(), name="register"),
